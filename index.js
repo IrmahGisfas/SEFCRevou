6cc2b236147e5630380d64512813649012d5809c
IrmahGisfas <103916958+IrmahGisfas@users.noreply.github.com>
2023-03-09 23:32:38 +0700

    Tugas Revou - SEFC

---
+++ index.js
@@ -0,0 +1,18 @@
+function clickcall() {
+    window.location.href="https://wa.me/6282134733546";
+}
+
+const home=document.getElementById('home')
+function scrolltohome() {
+    home.scrollIntoView();
+}
+
+const ourpackage=document.getElementById('ourpackage')
+function scrolltopakage() {
+    ourpackage.scrollIntoView({behavior:'smooth'});
+}
+
+const travelagent=document.getElementById('travelagent')
+function scrolltobottom() {
+    travelagent.scrollIntoView({behavior:'smooth'});
+}
